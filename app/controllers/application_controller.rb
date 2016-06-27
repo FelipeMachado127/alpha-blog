f14daf0f9e1176697e06c506003b82fe13be81b5
Rails Udemy Course <judgeflips@gmail.com>
2016-06-27 23:31:26 +0000

    fix list Categories test bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   helper_method :current_user, :logged_in?
+  layout proc{ |c| c.request.xhr? ? false : "application" }
   
   def current_user
    @current_user ||= User.find(session[:user_id]) if session[:user_id]
